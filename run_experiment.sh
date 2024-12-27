ad2232482d0d570fcd9b40f88290e41aa3d90431
Sebastian Eggers <sebastian.eggers@icloud.com>
2024-12-27 08:43:43 +0100

    Add draw param to mining experiment

--- run_experiment.sh
+++ run_experiment.sh
@@ -45,7 +45,7 @@ case $e_value in
         python main.py -g -e "watch" -n "$n_value"
         ;;
     mine_dataflows)
-        python main.py -g -e "mine_dataflows_on_kaggle_dataset"
+        python main.py -g -e "mine_dataflows_on_kaggle_dataset" -d
         ;;
     *)
         echo "Error: Invalid value for -e. Valid options are 'single_dataflow' or 'mine_dataflows'."
