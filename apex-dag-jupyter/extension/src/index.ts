ef3d8d0f1e6d82cf4ea9f27f162d12ae653b8932
Sebastian Eggers <sebastian.eggers@icloud.com>
2025-03-30 17:38:41 +0200

    Jupyter Plugin

--- apex-dag-jupyter/extension/src/index.ts
+++ apex-dag-jupyter/extension/src/index.ts
@@ -2,6 +2,14 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { MainAreaWidget } from '@jupyterlab/apputils';
+import { ILauncher } from '@jupyterlab/launcher';
+import { reactIcon } from '@jupyterlab/ui-components';
+import { GraphWidget } from './widget';
+
+namespace CommandIDs {
+  export const create = 'apex-dag-widget';
+}
 
 /**
  * Initialization data for the apex-dag extension.
@@ -10,8 +18,29 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'apex-dag:plugin',
   description: 'APEX-DAG Jupyter Lab Extension',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension apex-dag is activated!');
+  optional: [ILauncher],
+  activate: (app: JupyterFrontEnd, launcher: ILauncher) => {
+    const { commands } = app;
+
+    const command = CommandIDs.create;
+    commands.addCommand(command, {
+      caption: 'APEX-DAG',
+      label: 'APEX-DAG Widget',
+      icon: args => (args['isPalette'] ? undefined : reactIcon),
+      execute: () => {
+        const content = new GraphWidget();
+        const widget = new MainAreaWidget<GraphWidget>({ content });
+        widget.title.label = 'APEX-DAG';
+        widget.title.icon = reactIcon;
+        app.shell.add(widget, 'main');
+      }
+    });
+
+    if (launcher) {
+      launcher.add({
+        command
+      });
+    }
   }
 };
 
