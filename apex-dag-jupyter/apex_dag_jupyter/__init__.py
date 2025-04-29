b50ce6017cc225617ca6793bf0d8ed22f3739504
Sebastian Eggers <sebastian.eggers@icloud.com>
2025-04-29 07:53:01 +0200

    Jupyter server backend

--- apex-dag-jupyter/apex_dag_jupyter/__init__.py
+++ apex-dag-jupyter/apex_dag_jupyter/__init__.py
@@ -8,6 +8,8 @@ except ImportError:
     warnings.warn("Importing 'apex_dag_jupyter' outside a proper installation.")
     __version__ = "dev"
 
+from .handlers import setup_handlers
+
 
 def _jupyter_labextension_paths():
     return [{
@@ -16,47 +18,20 @@ def _jupyter_labextension_paths():
     }]
 
 
-import os
-import json
-
-from jupyter_server.base.handlers import APIHandler
-from jupyter_server.utils import url_path_join
-import tornado
-from tornado.web import StaticFileHandler
+def _jupyter_server_extension_points():
+    return [{
+        "module": "apex_dag_jupyter"
+    }]
 
 
-class RouteHandler(APIHandler):
-    # The following decorator should be present on all verb methods (head, get, post,
-    # patch, put, delete, options) to ensure only authorized user can request the
-    # Jupyter server
-    @tornado.web.authenticated
-    def get(self):
-        self.finish(json.dumps({
-            "data": "This is /jupyterlab-examples-server/hello endpoint!"
-        }))
+def _load_jupyter_server_extension(server_app):
+    """Registers the API handler to receive HTTP requests from the frontend extension.
 
-    @tornado.web.authenticated
-    def post(self):
-        # input_data is a dictionary with a key "name"
-        input_data = self.get_json_body()
-        data = {"greetings": "Hello {}, enjoy JupyterLab!".format(input_data["name"])}
-        self.finish(json.dumps(data))
-
-
-def setup_handlers(web_app):
-    host_pattern = ".*$"
-
-    base_url = web_app.settings["base_url"]
-    # Prepend the base_url so that it works in a JupyterHub setting
-    route_pattern = url_path_join(base_url, "jupyterlab-examples-server", "hello")
-    handlers = [(route_pattern, RouteHandler)]
-    web_app.add_handlers(host_pattern, handlers)
-
-    # Prepend the base_url so that it works in a JupyterHub setting
-    doc_url = url_path_join(base_url, "jupyterlab-examples-server", "public")
-    doc_dir = os.getenv(
-        "JLAB_SERVER_EXAMPLE_STATIC_DIR",
-        os.path.join(os.path.dirname(__file__), "public"),
-    )
-    handlers = [("{}/(.*)".format(doc_url), StaticFileHandler, {"path": doc_dir})]
-    web_app.add_handlers(host_pattern, handlers)
+    Parameters
+    ----------
+    server_app: jupyterlab.labapp.LabApp
+        JupyterLab application instance
+    """
+    setup_handlers(server_app.web_app)
+    name = "apex_dag_jupyter"
+    server_app.log.info(f"Registered {name} server extension")
