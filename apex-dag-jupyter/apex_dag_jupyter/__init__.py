40f8afcab9599b69c73805de14f4450f5ebed18d
Sebastian Eggers <sebastian.eggers@icloud.com>
2025-05-25 10:02:48 +0200

    Initialize empty model and graph encoder

--- apex-dag-jupyter/apex_dag_jupyter/__init__.py
+++ apex-dag-jupyter/apex_dag_jupyter/__init__.py
@@ -8,6 +8,12 @@ except ImportError:
     warnings.warn("Importing 'apex_dag_jupyter' outside a proper installation.")
     __version__ = "dev"
 
+import yaml
+import logging
+from pathlib import Path
+from ApexDAG.nn.gat import MultiTaskGAT
+from ApexDAG.nn.training import GraphEncoder, Modes
+from ApexDAG.util.training_utils import TASKS_PER_GRAPH_TRANSFORM_MODE_PRETRAIN
 from .handlers import setup_handlers
 
 
@@ -23,6 +29,37 @@ def _jupyter_server_extension_points():
         "module": "apex_dag_jupyter"
     }]
 
+def create_model(config, reversed, tasks):
+    return MultiTaskGAT(
+            hidden_dim=config["hidden_dim"], 
+            dim_embed=config["dim_embed"],
+            num_heads=config["num_heads"], 
+            edge_classes=config["node_classes"] if reversed else config["edge_classes"],
+            node_classes=config["edge_classes"]if reversed else config["node_classes"],
+            residual=config["residual"],
+            dropout=config["dropout"],
+            number_gat_blocks=config["number_gat_blocks"],
+            task = tasks
+        )
+
+
+def load_apex_model(config, logger: logging.Logger):
+    print("APEX-DAG Plugin: Initializing ML Model...")
+    # Ersetzen Sie dies durch Ihre tatsächliche Modelllade-Logik
+    # z.B. model = joblib.load('path/to/your/model.pkl')        
+    encoded_checkpoint_path = Path(config["encoded_checkpoint_path"])
+    graph_encoder = GraphEncoder(encoded_checkpoint_path, logger, 
+                                 config['min_nodes'], 
+                                 config['min_edges'], 
+                                 config['load_encoded_old_if_exist'],
+                                 mode = config["mode"])
+    
+    model = create_model(config, reversed = ('reversed' in config["mode"].value), tasks = TASKS_PER_GRAPH_TRANSFORM_MODE_PRETRAIN[config["mode"].value])
+    model.to(config['device'])
+
+    artifacts = {"encoder": graph_encoder, "model": model}
+    return artifacts
+
 
 def _load_jupyter_server_extension(server_app):
     """Registers the API handler to receive HTTP requests from the frontend extension.
@@ -32,6 +69,12 @@ def _load_jupyter_server_extension(server_app):
     server_app: jupyterlab.labapp.LabApp
         JupyterLab application instance
     """
-    setup_handlers(server_app.web_app)
+    global ML_MODEL_INSTANCE
+    if ML_MODEL_INSTANCE is None:
+        ML_MODEL_INSTANCE = load_apex_model(server_app.config, server_app.log)
+    else:
+        server_app.log.info("APEX-DAG Plugin: ML Model already loaded.")
+
+    setup_handlers(server_app, ML_MODEL_INSTANCE, server_app.config)
     name = "apex_dag_jupyter"
     server_app.log.info(f"Registered {name} server extension")
--- apex-dag-jupyter/apex_dag_jupyter/handlers.py
+++ apex-dag-jupyter/apex_dag_jupyter/handlers.py
@@ -7,7 +7,12 @@ from jupyter_server.utils import url_path_join
 from ApexDAG.sca.py_data_flow_graph import PythonDataFlowGraph as DataFlowGraph
 
 
+
 class DataflowHandler(APIHandler):
+    def initialize(self, model, jupyter_server_app_config=None):
+        self.model = model
+        self.jupyter_server_app_config = jupyter_server_app_config
+
     @tornado.web.authenticated
     def post(self):
         input_data = self.get_json_body()
@@ -42,23 +47,44 @@ class DataflowHandler(APIHandler):
 
 
 class LineageHandler(APIHandler):
+    def initialize(self, model, jupyter_server_app_config=None):
+        self.model = model
+        self.jupyter_server_app_config = jupyter_server_app_config
+
     @tornado.web.authenticated
     def post(self):
         input_data = self.get_json_body()
+        code = input_data["code"]
 
-        result = {
-            "message": "Processed lineage successfully!",
-            "input": input_data
-        }
+        dfg = DataFlowGraph()
+        try:
+            dfg.parse_code(code)
+        except SyntaxError as e:
+            print(f"SyntaxError: {e}")
+            result = {
+                "message": "Cannot process dataflow!",
+                "success": False,
+                "dataflow": {}
+            }
+            self.finish(json.dumps(result))
+        else:
+            dfg.optimize()
+            graph_json = dfg.to_json()
 
-        self.finish(json.dumps(result))
+            result = {
+                "message": "Processed dataflow successfully!",
+                "success": True,
+                "dataflow": graph_json
+            }
+
+            self.finish(json.dumps(result))
 
     def data_received(self, chunk):
         """Override to silence Tornado abstract method warning."""
         pass
 
 
-def setup_handlers(web_app):
+def setup_handlers(web_app, model_instance, jupyter_server_app_config=None):
     host_pattern = ".*$"
 
     base_url = web_app.settings["base_url"]
@@ -66,8 +92,8 @@ def setup_handlers(web_app):
     dataflow_pattern = url_path_join(base_url, "apex-dag", "dataflow")
     lineage_pattern = url_path_join(base_url, "apex-dag", "lineage")
     handlers = [
-        (dataflow_pattern, DataflowHandler),
-        (lineage_pattern, LineageHandler),
+        (dataflow_pattern, DataflowHandler, dict(model=model_instance, jupyter_server_app_config=jupyter_server_app_config)),
+        (lineage_pattern, LineageHandler, dict(model=model_instance, jupyter_server_app_config=jupyter_server_app_config)),
     ]
 
     web_app.add_handlers(host_pattern, handlers)
