f3f8326176e3bfeb5ae790d87a3922eccd61f496
Sebastian Eggers <sebastian.eggers@e-p-digital.de>
2025-06-22 20:23:57 +0200

    Cleanup

--- run_watch.sh
+++
@@ -1,5 +0,0 @@
-node ./apex-dag-jupyter/src/app/module/file_watcher.js ./apex-dag-jupyter/src/app/data_flow_graph.json &
-python main.py -e watch -n "$1" &
-cd ./apex-dag-jupyter/src/app && npm run dev &
-
-wait
